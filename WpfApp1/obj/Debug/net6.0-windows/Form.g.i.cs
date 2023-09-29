5cedc4f04dad0767ea1d0bca57cc985798884941
JosueeeC <josuecastillo_m@hotmail.com>
2023-09-28 21:56:14 -0500

    1.0

--- WpfApp1/obj/Debug/net6.0-windows/Form.g.i.cs
+++ WpfApp1/obj/Debug/net6.0-windows/Form.g.i.cs
@@ -141,7 +141,7 @@ namespace WpfApp1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/form.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/form.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Form.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
